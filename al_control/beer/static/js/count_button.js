6f0cf2fa9ec04f0d0f7195b20a8f80019bd0f7c8
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-07-21 08:49:19 +0900

    書き方修正part2

--- al_control/beer/static/js/count_button.js
+++ al_control/beer/static/js/count_button.js
@@ -1,54 +1,54 @@
-// (() => {
-//   //HTMLのid値を使って以下のDOM要素を取得
-//   const downbutton = document.getElementById('down');
-//   const upbutton = document.getElementById('up');
-//   const text = document.getElementById('textbox');
-//   const reset = document.getElementById('reset');
+(() => {
+  //HTMLのid値を使って以下のDOM要素を取得
+  const downbutton = document.getElementById('down');
+  const upbutton = document.getElementById('up');
+  const text = document.getElementById('textbox');
+  const reset = document.getElementById('reset');
 
-//   //ボタンが押されたらカウント減
-//   downbutton.addEventListener('click', (event) => {
-//   //0以下にはならないようにする
-//   if(text.value >= 1) {
-//     text.value--;
-//   }
+  //ボタンが押されたらカウント減
+  downbutton.addEventListener('click', (event) => {
+  //0以下にはならないようにする
+  if(text.value >= 1) {
+    text.value--;
+  }
+  });
+
+  //ボタンが押されたらカウント増
+  upbutton.addEventListener('click', (event) => {
+    text.value++;
+  })
+
+  //ボタンが押されたら0に戻る
+  reset.addEventListener('click', (event) => {
+    text.value = 0;
+  })
+
+})();
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const downButtons = document.querySelectorAll('.down');
+//   const upButtons = document.querySelectorAll('.up');
+//   const textboxes = document.querySelectorAll('.count');
+//   const resetButtons = document.querySelectorAll('.resetbtn');
+
+//   downButtons.forEach((downButton, index) => {
+//     downButton.addEventListener('click', () => {
+//       if (textboxes[index].value >= 1) {
+//         textboxes[index].value--;
+//       }
+//     });
 //   });
 
-//   //ボタンが押されたらカウント増
-//   upbutton.addEventListener('click', (event) => {
-//     text.value++;
-//   })
+//   upButtons.forEach((upButton, index) => {
+//     upButton.addEventListener('click', () => {
+//       textboxes[index].value++;
+//     });
+//   });
 
-//   //ボタンが押されたら0に戻る
-//   reset.addEventListener('click', (event) => {
-//     text.value = 0;
-//   })
-
-// })();
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const downButtons = document.querySelectorAll('.down');
-  const upButtons = document.querySelectorAll('.up');
-  const textboxes = document.querySelectorAll('.count');
-  const resetButtons = document.querySelectorAll('.resetbtn');
-
-  downButtons.forEach((downButton, index) => {
-    downButton.addEventListener('click', () => {
-      if (textboxes[index].value >= 1) {
-        textboxes[index].value--;
-      }
-    });
-  });
-
-  upButtons.forEach((upButton, index) => {
-    upButton.addEventListener('click', () => {
-      textboxes[index].value++;
-    });
-  });
-
-  resetButtons.forEach((resetButton, index) => {
-    resetButton.addEventListener('click', () => {
-      textboxes[index].value = 0;
-    });
-  });
-});
+//   resetButtons.forEach((resetButton, index) => {
+//     resetButton.addEventListener('click', () => {
+//       textboxes[index].value = 0;
+//     });
+//   });
+// });
